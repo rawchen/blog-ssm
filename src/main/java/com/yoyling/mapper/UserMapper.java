10ad1676e7534629338dfa87f1bc1da624574d6e
yoyling <2221999792@qq.com>
2021-01-08 21:33:30 +0800

    自动生成User

---
+++ src/main/java/com/yoyling/mapper/UserMapper.java
@@ -0,0 +1,19 @@
+package com.yoyling.mapper;
+
+import com.yoyling.domain.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer uid);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer uid);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+
+    User selectUserByNameAndPassword(User u);
+}
